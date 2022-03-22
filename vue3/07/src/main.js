4a598669bac6a0ae400cbfa4aea38e22b68615b4
Spike <zhangzuoxuan@gmail.com>
2022-03-23 00:26:47 +0800

    update

--- vue3/07/src/main.js
+++ vue3/07/src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-02-25 15:24:29
- * @LastEditTime: 2022-02-25 16:18:50
+ * @LastEditTime: 2022-03-11 15:02:19
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue3/07/src/main.js
